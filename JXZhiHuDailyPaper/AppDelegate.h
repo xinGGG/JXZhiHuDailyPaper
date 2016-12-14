3620f533b1bf738a623c39ad8185685f384550c3
xinGGG <317033640@qq.com>
2016-12-14 20:54:30 +0800

    setup project

---
+++ JXZhiHuDailyPaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXZhiHuDailyPaper
+//
+//  Created by xing on 16/12/14.
+//  Copyright © 2016年 ljx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '7.0'
+pod 'PureLayout'
+pod 'pop'
+pod 'AFNetworking'
+pod 'IQKeyboardManager'
+pod 'SDWebImage', '~>3.6'
+pod 'YYKit'
